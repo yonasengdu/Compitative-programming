2f8d0423402927a370eddfbea429aa9257cab059
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2022-11-13 08:52:52 +0300

    Time: 34 ms (27.40%), Space: 13.4 MB (35.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def climbStairs(self, n):
+        one,two = 1,1
+        for i in range(n-1):
+            tmp = one
+            one = one + two
+            two = tmp
+            
+        return one
+        
