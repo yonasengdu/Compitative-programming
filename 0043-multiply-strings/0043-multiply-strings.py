72bc8a5907bb712d6ccfde3f0129b36962f240ef
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2022-12-26 15:24:48 +0300

    Time: 41 ms (86.00%), Space: 14 MB (28.85%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        decoder ={'0':0,'1':1 ,'2':2 ,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+        int_num1 = 0
+        int_num2 = 0
+        
+        for i in range(len(num1)):
+            decoded_num1 = decoder[num1[~i]]
+            int_num1 += ((10**i)*(decoded_num1))
+                         
+        for j in range(len(num2)):
+                         decoded_num2 = decoder[num2[~j]]
+                         int_num2 += ((10**j)*(decoded_num2))
+        print(int_num1, int_num2)
+                         
+        return str(int_num1*int_num2)
