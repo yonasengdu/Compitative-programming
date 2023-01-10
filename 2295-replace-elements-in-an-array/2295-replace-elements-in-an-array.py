c1a8e42129d4b6a741453a22e572230b9bd19867
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2023-01-10 13:35:13 +0300

    Time: 1442 ms (75.36%), Space: 68.6 MB (79.64%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        num_index = {}
+        for index in range(len(nums)):
+            num_index[nums[index]] = index
+        for old,new in operations:
+            index = num_index[old]
+            nums[index] = new
+            num_index[new] = num_index.pop(old)
+        return nums
+            
+        
+             
+        
+        
