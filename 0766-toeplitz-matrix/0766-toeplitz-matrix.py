eef7c2f7bfb2c225edf5f80bf42399196fc1ff93
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2023-01-22 00:13:02 +0300

    Time: 132 ms (29.32%), Space: 13.8 MB (70.35%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,20 +1,10 @@
 class Solution:
-    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        
-        for row in range(len(matrix)):
-            for ind in range(len(matrix[row])):
-                rind = row
-                cind = ind
-                diff = abs(rind - cind)
-                diagonal = matrix[rind][cind]
-                while rind < len(matrix) and cind < len(matrix[row]):
-                    if abs(rind - cind)  == diff and matrix[rind][cind] != diagonal:
-                        return False
-                    rind += 1
-                    cind += 1
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:  
+        for row in range(len(matrix)-1):
+            for ind in range(len(matrix[row])-1):
+                if matrix[row][ind] != matrix[row+1][ind+1]:
+                    return False
         return True
-
-                                
-                         
+                 
                          
             
