d8230b24b486d8bdcdbea6ede041120d1eb658c9
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2023-01-28 04:06:36 +0300

    Time: 118 ms (37.21%), Space: 13.9 MB (29.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,41 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-            rows = [set() for x in range(9)]
-            columns = [set() for x in range(9)]
-            squares = [[set() for x in range(3)] for y in range(3)]
+            def checker(row,col):
+                new_arr = []
+                val = [(0,0),(0,1),(0,2),(1,0),(2,0),(1,1),(1,2),(2,1),(2,2)]
+                for i,j in val:
+                    newRow = row + i
+                    newCol = col + j
+                    new_arr.append(board[newRow][newCol])
+                count_new_arr = Counter(new_arr)
+                for val,num in count_new_arr.items():
+                    if val != "." and num > 1:
+                        return True
+                return False
 
-            for x in range(9):
-                for y in range(9):
-                    cell_value = board[x][y]
-                    if cell_value == ".":
-                        continue
-                    if cell_value in rows[x] or cell_value in columns[y] or cell_value in squares[x//3][y//3]:
+            for ind in range(9):
+                row = board[ind]
+                col = []
+                for jnd in range(9):
+                    col.append(board[jnd][ind])
+                count_row = Counter(row)
+                count_col = Counter(col)
+                for val,num in count_row.items():
+                    if val != "." and num > 1:
+                        return False
+                for val,num in count_col.items():
+                    if val != "." and num > 1:
                         return False
 
-                    rows[x].add(cell_value)
-                    columns[y].add(cell_value)
-                    squares[x//3][y//3].add(cell_value)
-
+            for ind in range(0,9,3):
+                if checker(0,ind) or checker(3,ind) or checker(6,ind):
+                    return False
             return True
+                
+                
+                
+          
+
+            
+            
+                
