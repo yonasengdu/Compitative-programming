ab42c951cd77f05899b8b62d515ec028f99b1af5
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2023-02-21 14:58:31 +0300

    Time: 709 ms (94.74%), Space: 31.1 MB (98.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,12 +3,46 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+def traversAnddCheck(left,right):
+    while left and right:
+        if left.val != right.val:
+            return False
+        left = left.next
+        right = right.next
+    if not left and not right:
+        return True
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        val = []
+        count = 0
         curr = head
+       
         while curr:
-            val.append(curr.val)
+            count += 1
             curr = curr.next
-        return val == val[::-1]
+        
+        prev = None
+        curr = head
+        if count % 2 == 0 :
+            for i in range(count//2):
+                    temp = curr.next
+                    curr.next = prev
+                    prev = curr
+                    curr = temp
+        else:
+            for i in range(count//2):
+               
+                    temp = curr.next
+                    curr.next = prev
+                    prev = curr
+                    curr= temp
+            curr = curr.next
+        return traversAnddCheck(prev,curr)
+       
+            
+            
+            
+                    
+                
+                
+        
         
