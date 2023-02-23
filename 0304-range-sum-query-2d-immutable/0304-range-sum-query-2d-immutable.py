5372980c020cc10434027a26431b7e9edd136ef9
yonasengdu <101667631+yonasengdu@users.noreply.github.com>
2023-02-23 11:09:26 +0300

    Time: 1567 ms (30.86%), Space: 25.2 MB (23.32%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -9,9 +9,7 @@ class NumMatrix:
         for row in range(1,len(self.matrix)):
             for col in range(1,len(self.matrix[0])):
                 self.matrix[row][col] = self.matrix[row][col] + self.matrix[row][col-1] + self.matrix[row -1][col] - self.matrix[row-1][col-1]
-                
-        print(self.matrix)
-                
+     
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
