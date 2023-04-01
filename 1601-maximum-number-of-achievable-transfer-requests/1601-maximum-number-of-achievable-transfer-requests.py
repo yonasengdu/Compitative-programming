e8db432cebccc0d74ae79c32da1e1a5c95457e92
yonasengdu <engduyonas1994@gmail.com>
2023-04-01 20:41:57 +0300

    Time: 9403 ms (5.61%), Space: 16.5 MB (7.48%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,49 @@
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        self.comb_arr = []
+        self.ans = 0
+        
+        def backtrack(start,comb,k):
+            if len(comb) == k:
+                self.comb_arr.append(comb[:])
+                return
+            if start == len(requests):
+                return
+            
+            for num in range(start,len(requests)):
+                comb.append(requests[num])
+                
+                backtrack(num+1,comb,k)
+                
+                comb.pop()
+                
+        def checker(arr):
+            
+            
+            for req_comb in arr:
+                validator_dict = defaultdict(int)
+                for req in req_comb:
+                    fromm = req[0]
+                    to = req[1]
+                    
+                    validator_dict[fromm] -= 1
+                    validator_dict[to] += 1
+                flag = True  
+                for val in validator_dict.values():
+                    if val != 0:
+                        flag = False
+                        
+                if flag:
+                    self.ans = max(self.ans,len(req_comb))
+                    
+                    
+        
+        for k in range(len(requests)+1):
+            backtrack(0,[],k)
+            checker(self.comb_arr)
+            self.comb_arr = []
+            
+            
+            
+        return self.ans
+        
