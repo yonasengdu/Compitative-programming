e35cfc00518531ff2e78d8a53254abdd2c84113f
yonasengdu <engduyonas1994@gmail.com>
2023-05-09 02:54:25 +0300

    Time: 105 ms (49.98%), Space: 17.7 MB (23.67%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> list:
+    
+        courses = [[] for _ in range(numCourses)]
+        colors = [0 for _ in range(numCourses) ] 
+        
+        for take,pre in prerequisites:
+            courses[pre].append(take)
+            colors[take] += 1
+            
+            
+        ans = []    
+        queue = deque()
+        
+        for ind,node in enumerate(colors):
+            if not node:
+                queue.append(ind)
+            
+
+                
+        while queue:
+            curr = queue.popleft()
+            
+            ans.append(curr)
+            
+            for child in courses[curr]:
+                colors[child] -= 1
+                if colors[child] == 0:
+                    queue.append(child)
+      
+    
+        if len(ans) != numCourses:
+            return []
+        
+        return ans
+            
