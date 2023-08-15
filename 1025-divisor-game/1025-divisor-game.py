4c40720c62969cdb6015505bb96c424606fe7f7d
yonasengdu <engduyonas1994@gmail.com>
2023-08-15 23:21:42 +0300

    Time: 42 ms (66.62%), Space: 16.2 MB (89.87%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,7 +1,5 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        if n % 2 == 0:
-            return True
-        else:
-            return False
+        return n % 2 == 0
+         
         
