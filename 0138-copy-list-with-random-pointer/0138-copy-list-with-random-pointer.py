5bd632a0c2af6cffd435a6944da16ddf4aecab8f
yonasengdu <engduyonas1994@gmail.com>
2023-09-05 15:02:42 +0300

    Time: 32 ms (98.27%), Space: 17.4 MB (37.47%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,5 +9,28 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        return deepcopy(head)
+        copy_dict = {}
+        
+        
+        
+        curr = head
+        
+        while curr:
+            copy_dict[curr] = Node(curr.val)
+            curr = curr.next
+            
+        for node,cp in copy_dict.items():
+            
+            if node.next:
+                cp.next = copy_dict[node.next]
+                
+            if node.random:
+                cp.random =  copy_dict[node.random]
+            
+            
+           
+                                
+        
+        return copy_dict[head] if head else None
+       
         
