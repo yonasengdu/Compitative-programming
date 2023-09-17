bbc383367d84f43ac8d393905bd1e5be15a52b19
yonasengdu <engduyonas1994@gmail.com>
2023-09-17 12:49:38 -0700

    Time: 161 ms (75.00%), Space: 21.2 MB (71.88%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
@@ -1,46 +1,59 @@
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        queue = deque([(1 << i, i, 0) for i in range(n)])
+        visited = set((1 << i, i) for i in range(n))
+        
+        while queue:
+            mask, node, dist = queue.popleft()
+            if mask == (1 << n) - 1:
+                return dist
+            for neighbor in graph[node]:
+                new_mask = mask | (1 << neighbor)
+                if (new_mask, neighbor) not in visited:
+                    visited.add((new_mask, neighbor))
+                    queue.append((new_mask, neighbor, dist + 1))
         
         
-        def bfs(node):
+#         def bfs(node):
            
           
-            N = len(graph)
-            target = ( 2 ** N) - 1
+#             N = len(graph)
+#             target = ( 2 ** N) - 1
             
-            visited = set()
-            queue = deque()
+#             visited = set()
+#             queue = deque()
             
-            queue.append((node,0,0))
-            visited.add((node,0))
+#             queue.append((node,0,0))
+#             visited.add((node,0))
             
             
-            while queue:
+#             while queue:
              
-                curr_node,curr_mask,curr_level = queue.popleft()
+#                 curr_node,curr_mask,curr_level = queue.popleft()
                 
               
-                if curr_mask == target:
-                    return curr_level
+#                 if curr_mask == target:
+#                     return curr_level
                     
                
-                new_mask = ( 1 << curr_node | curr_mask)
+#                 new_mask = ( 1 << curr_node | curr_mask)
               
-                for neigh in graph[curr_node]:
-                    if (neigh,new_mask) not in visited:
-                        visited.add((neigh,new_mask))
-                        queue.append((neigh,new_mask,curr_level + 1))
+#                 for neigh in graph[curr_node]:
+#                     if (neigh,new_mask) not in visited:
+#                         visited.add((neigh,new_mask))
+#                         queue.append((neigh,new_mask,curr_level + 1))
                         
                         
-            return 1
+#             return 1
                         
                 
                         
-        path = float("inf")                
-        for i in range(len(graph)):
-            path = min(path,bfs(i))
+#         path = float("inf")                
+#         for i in range(len(graph)):
+#             path = min(path,bfs(i))
             
-        return path - 1
+#         return path - 1
             
                         
                 
