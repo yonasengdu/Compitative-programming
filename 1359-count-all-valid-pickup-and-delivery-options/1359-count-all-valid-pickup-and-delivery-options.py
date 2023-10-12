b5907189daf653b61c616d674d2000d332c69495
yonasengdu <engduyonas1994@gmail.com>
2023-10-12 17:57:50 +0300

    Time: 22 ms (99.93%), Space: 16.2 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,16 +1,5 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        dp = [0]*n
-        dp[0] = 1
-     
-        
-        for i in range(2,n+1):
-            n = 2*(i - 1) + 1
-            curr_count = (n * (n + 1))// 2
-           
-            dp[i - 1] += (dp[i - 2] * curr_count)
-            
-        
-        return dp[-1] % (10**9 + 7)
-            
-            
+        return (factorial(2*n) // (2**n)) % (10**9 + 7)
+
+    
