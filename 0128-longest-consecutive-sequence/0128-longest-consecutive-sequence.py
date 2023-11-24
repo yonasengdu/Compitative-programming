5e55e2e4ad884c7fa60206d3827ee59540d8ba70
yonasengdu <engduyonas1994@gmail.com>
2023-11-24 18:43:23 +0300

    Time: 858 ms (36.59%), Space: 138.4 MB (5.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,29 +1,50 @@
+class UF():
+    def __init__(self,nums):
+        self.graph = {par:par for par in nums}
+        
+        
+        
+    def find(self,x):
+        if self.graph[x] != x:
+            self.graph[x] = self.find(self.graph[x])
+            
+        return self.graph[x]
+    
+    def union(self,x,y):
+        repX = self.find(x)
+        repY = self.find(y)
+        
+        self.graph[repX] = repY
+        
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        uf = UF(nums)
+        nums = Counter(nums)
         
-        nums = set(nums)
         
-        ans = 0
+        
+        for num,count in nums.items():
+            if  num + 1 in nums:
+                uf.union(num,num + 1)
+                
+                
+            if num - 1 in nums:
+                uf.union(num, num - 1)
+                
         for num in nums:
-            if num - 1 not in nums:
-                consequetives = num + 1
-                temp = 1
-                while consequetives in nums:
-                    consequetives += 1
-                    temp += 1
-                    
-                ans = max(ans,temp)
+            uf.find(num)
                 
-                
-        return ans
-                    
-                
-    
-            
-            
-                
-                
-            
-            
-
         
+        ss_length = defaultdict(int)
+        ss_length[0] = 0
+        
+        for ID in uf.graph.values():
+                ss_length[ID] += 1
+                
+        return max(ss_length.values())
+                
+        
+                
+        
+        
+      
